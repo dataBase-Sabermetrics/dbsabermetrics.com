bd1312ab5f978ae4b3e66f119d46a1c22ec5d5e6
drewbeno1 <benodrew12@gmail.com>
2024-05-03 14:14:50 -0400

    fix bad import

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-  </div>
   <HelloWorld msg="dataBase Sabermetrics" />
 </template>
 
