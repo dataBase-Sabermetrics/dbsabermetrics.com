5c16601d05f5cb2294747043604deddf0716aace
drewbeno1 <benodrew12@gmail.com>
2024-05-18 13:28:27 -0400

    include raj font

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import './style.css'
+import '@fontsource/rajdhani/latin.css';
 
 createApp(App)
   .use(router)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{vue,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rajdhani: ['Rajdhani', ...defaultTheme.fontFamily.sans],
+    },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
