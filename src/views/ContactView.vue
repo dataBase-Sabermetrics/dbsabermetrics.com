9d7b545652d0fedf042a50dded9e31ac5f5e20b0
drewbeno1 <benodrew12@gmail.com>
2024-05-30 16:50:21 -0400

    remove phone number

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -88,21 +88,6 @@ const handleSubmit = (e) => {
                 </div>
             </div>
             <div class="sm:col-span-2">
-                <label for="phone" class="block text-sm font-semibold leading-6 text-gray-300">Phone</label>
-                <div class="relative mt-2.5">
-                <div class="absolute inset-y-0 left-0 flex items-center ">
-                    <label for="country" class="sr-only">Country</label>
-                    <select id="country" name="country" class="h-full rounded-md border-0 bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                    <option>US</option>
-                    <option>CA</option>
-                    <option>EU</option>
-                    </select>
-                    <ChevronDownIcon class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400" aria-hidden="true" />
-                </div>
-                <input v-model="formData.phone" type="tel" name="phone" id="phone" autocomplete="tel" class="block bg-gray-200/50 w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                </div>
-            </div>
-            <div class="sm:col-span-2">
                 <label for="message" class="block text-sm font-semibold leading-6 text-gray-300">Message</label>
                 <div class="mt-2.5">
                 <textarea v-model="formData.message" name="message" id="message" rows="4" class="block bg-gray-200/50 w-full rounded-md border-0 px-3.5 py-2 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-900 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
