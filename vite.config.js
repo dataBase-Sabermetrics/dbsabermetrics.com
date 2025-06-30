3bcc344bbfd4025cce4e11c41a8ce84850447cbc
Andrew Beno <benodrew12@gmail.com>
2025-06-29 22:26:48 -0400

    update esteban path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://cyber.drewbeno.com',
+        target: 'http://esteban.drewbeno.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
